f288325667c6aa3bc9116027fba932c280f8a3fb
Lev2307 <levchik23072007@gmail.com>
2022-06-24 18:30:33 +0300

    upd

--- file_manager.py
+++ file_manager.py
@@ -1,8 +1,28 @@
 import pickle
 
 class FileManager:
-    def load_data(self):
-        pass
+    def load_data(self, name):
+        with open(f'{name}.bin', 'rb') as f:
+            p = pickle.loads(f)
+        return p
     
-    def save_data(delf):
-        pass
+    def save_data(self, data):
+        new_data = data
+        level_int = new_data['level']
+        with open(f'level_{level_int}.bin', 'wb') as f:
+            pickle.dumps(f)
+
+test_data = {
+    'level': 1,
+    'obstacles': {
+        'easy': 30,
+        'medium': 15,
+        'hard': 5
+    },
+    'word': 'orange',
+}
+f = FileManager()
+
+f.save_data(test_data)
+
+f.load_data('level_1')
